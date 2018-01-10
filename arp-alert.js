ac38a322db5c6d0ab93bc2ef1a5c8774d3073881
Carlos Rodriguez <carlos@rodriguezst.es>
2018-01-10 20:32:20 +0100

    Typos

--- arp-alert.js
+++ arp-alert.js
@@ -1,17 +1,18 @@
 var fs = require('fs');
+const ArpMonitor = require('arp-monitor');
 
 var known_hosts = fs.readFileSync('known_hosts.txt');
 var arpMonitor = new ArpMonitor();
 
 function alert(node) {
     console.log(node.mac, node.ip);
-	known_hosts += (node.mac + ',')
-	fs.appendFileSync('known_hosts.txt', node.mac + ',');
+    known_hosts += (node.mac + ',');
+    fs.appendFileSync('known_hosts.txt', node.mac + ',');
 }
  
 arpMonitor.on("in", function(node) {
 	if(known_hosts.indexOf(node.mac) == -1) {
 		alert(node);
 	}
-}
+})
 
