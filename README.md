18def5d75ebf580756eb5656a31e33ad4a5d67d0
Carlos Rodriguez <carlos@rodriguezst.es>
2018-01-10 21:08:42 +0100

    Added Telegram BOT notification using HTTPS module.

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # arp-alert
-Monitors the ARP table and saves a list of known hosts. If a new client is detected (not stored on the list), an action is triggered and the client is added to the list.
+Monitors the ARP table and saves a list of known hosts. If a new client is detected (not stored on the list), a notification is sent using [Telegram BOT APIs](https://core.telegram.org/bots) and the client is added to the list.
 
-Change console.log(node.mac, node.ip); to the action you want to trigger when a new host is connected.
+### USAGE:
+Change **bot_api_key** and **client_key** in **arp_alert.js** to match your Telegram BOT values.
+
+```javascript
+const bot_api_key = 'bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
+const client_key = '00000000';
+```
--- arp-alert.js
+++ arp-alert.js
@@ -1,13 +1,22 @@
 var fs = require('fs');
+var https = require('https');
 const ArpMonitor = require('arp-monitor');
 
-var known_hosts = fs.readFileSync('known_hosts.txt');
+const bot_api_key = 'bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11';
+const client_key = '00000000';
+
+var hosts_file = 'known_hosts.txt';
+# Create file if not found
+fs.appendFileSync(hosts_file,'');
+var known_hosts = fs.readFileSync(hosts_file);
 var arpMonitor = new ArpMonitor();
 
 function alert(node) {
-    console.log(node.mac, node.ip);
+    var url = 'https://api.telegram.org/' + bot_api_key + '/sendMessage?chat_id=' + client_key + '&text=Client connected with MAC: ' + node.mac + ' and IP: ' + node.ip;
+    https.get(url);
+    console.log('[ARP-ALERT] New client connected: ',node.mac, node.ip);
     known_hosts += (node.mac + ',');
-    fs.appendFileSync('known_hosts.txt', node.mac + ',');
+    fs.appendFileSync(hosts_file, node.mac + ',');
 }
  
 arpMonitor.on("in", function(node) {
